c518c500254546c7966ed73eba3f5ac2470ae3a0
Nicolas Sanjaya <nicolas.sanjaya701@gmail.com>
2022-06-24 00:52:22 +0700

    Updates

---
+++ precache-manifest.8493b7e2db2b686cad0fbaf6ea19c3df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186943ddbeb80b986e1fe73dfcd8496a",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "33bba198da1cb255b11a",
+    "url": "/todo-list/static/css/main.70af190c.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/todo-list/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "33bba198da1cb255b11a",
+    "url": "/todo-list/static/js/main.7778d794.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
