964d22e7e7fceffd3daa9ddb706cf3a6d928c8a1
Nicolas Sanjaya <nicolas.sanjaya701@gmail.com>
2022-06-24 00:58:19 +0700

    update

--- src/inputBox.js
+++ src/inputBox.js
@@ -3,24 +3,29 @@ import React from "react";
 const inputBox = props => {
   const { inputValue, clickHandler, changeHandler } = props;
   return (
-    <div>
-      <label>Todo list</label>
-      <br />
-      <input
-        type="text"
-        value={inputValue}
-        name="inputValue"
-        onChange={changeHandler}
-        className="custom-input"
-      />
-      <br />
-      <button
-        className="custom-button"
-        onClick={e => clickHandler(e, inputValue)}
-      >
-        Add Todo List
-      </button>
-    </div>
+    <>
+      <div>
+        <label>Aplikasi Todo List</label>
+        <br />
+        <input
+          type="text"
+          value={inputValue}
+          name="inputValue"
+          onChange={changeHandler}
+          className="custom-input"
+        />
+        <br />
+        <button
+          className="custom-button"
+          onClick={e => clickHandler(e, inputValue)}
+        >
+          Tambahkan Todo List
+        </button>
+      </div>
+      <div>
+        <label>Nicolas Sanjaya</label>
+      </div>
+    </>
   );
 };
 
